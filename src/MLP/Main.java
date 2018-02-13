fd21381b406940f51e2613fa64d0792e7baee93b
Ben Diliberto <b.diliberto-14@student.lboro.ac.uk>
2018-02-13 16:34:50 +0000

    Forward pass completed

--- src/MLP/Main.java
+++ src/MLP/Main.java
@@ -9,6 +9,17 @@ public class Main {
 
         // TODO: Cleanse and seperate data
 
-        Network network = new Network(2, 3, 1);
+        Network network = new Network(5, 3, 1);
+
+        double[] attributes = new double[5];
+        attributes[0] = 7.4;
+        attributes[1] = 2.127;
+        attributes[2] = 121.4;
+        attributes[3] = 101.0;
+        attributes[4] = 81.0;
+
+        double predictand = 0.8;
+
+        network.trainNetwork(attributes, predictand);
     }
 }
--- src/MLP/Network.java
+++ src/MLP/Network.java
@@ -13,20 +13,10 @@ public class Network {
     private double learningRate = 0.1;
 
     /**
-     * Sets the learning rate for the Network
-     *
-     * @param learningRate The desired learning rate
-     */
-    public void setLearningRate(double learningRate) {
-        this.learningRate = learningRate;
-    }
-
-    /**
      * Create instance of Network and initialises
      *
      * @param input Number of Input nodes
      * @param hidden Number of Hidden nodes
-     * @param output Number of Output nodes
      */
     public Network(int input, int hidden, int output) {
         this.input = input;
@@ -49,7 +39,7 @@ public class Network {
      */
     private void randomiseWeights(){
         Random randDouble = new Random();
-        double wgtRange = (double)2/input;
+        double wgtRange = 2.0/input;
 
 
         for(int j = 1; j <= hidden; j++){
@@ -63,5 +53,60 @@ public class Network {
                 weightsHO[j][i] = randDouble.nextDouble() * (wgtRange * 2) - wgtRange; // Will generate random double between -2/n and 2/n for each weight
             }
         }
+    }
+
+    /**
+     * Sets the learning rate for the Network
+     *
+     * @param learningRate The desired learning rate
+     */
+    public void setLearningRate(double learningRate) {
+        this.learningRate = learningRate;
+    }
+
+    /**
+     * Trains the network for one cycle using the attributes supplied.
+     * Then back propogation is performed using the desired predictand supplied.
+     *
+     * @param attributes The attributes in the data used to train
+     * @param predictand The desired output from the attributes
+     * @return
+     */
+    public double[] trainNetwork(double[] attributes, double predictand) {
+        double[] output = forwardPass(attributes);
+        //backPropogation(predictand);
+
+        return output;
+    }
+
+    private double[] forwardPass(double[] attributes) {
+        for(int i = 0; i < input; i++){
+            inputs[i+1] = attributes[i];
+        }
+
+        inputs[0] = 1.0;
+        hiddens[0] = 1.0;
+
+        for(int j = 1; j <= hidden; j++){
+            hiddens[j] = 0.0;
+
+            for(int i = 0; i <= input; i++){
+                hiddens[j] += weightsIH[j][i] * inputs[i];
+            }
+
+            hiddens[j] = 1.0/(1.0+Math.exp(-hiddens[j]));
+        }
+
+        for(int j = 1; j <= output; j++){
+            outputs[j] = 0.0;
+
+            for(int i = 0; i <= hidden; i++){
+                outputs[j] += weightsHO[j][i] * hiddens[i];
+            }
+
+            outputs[j] = 1.0/(1.0+Math.exp(-outputs[j]));
+        }
+
+        return outputs;
     }
 }
